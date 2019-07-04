1e08384795731aad18e8355a0570dad36bb26bf7
IvSergey <imas.spb@mail.ru>
2019-07-04 13:07:57 +0300

    slider fixed

--- build/js/index.js
+++ build/js/index.js
@@ -1,1 +1,21 @@
 webpackJsonp([0],[function(n,c,t){"use strict";t(1),t(2)},function(n,c){},function(n,c){}],[0]);
+
+$(document).ready(function(){
+      $('.bxslider').bxSlider ({
+        
+      });
+    
+    });
+
+    const btn = document.querySelector('#btn');
+    const competition = document.querySelector('.competition__list');
+    
+    btn.addEventListener('click', ()=>{
+
+        if(competition.classList.contains("competition__list--active")){
+            competition.classList.remove("competition__list--active"); 
+        }
+        else{
+            competition.classList.add("competition__list--active");
+        }
+    })
